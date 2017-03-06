7d813cd4306d0d9c490c707f714b49c88551eb6e
zah <zhanah@ru-you.com>
2017-03-06 18:51:46 +0800

    init

--- app/src/main/java/com/zhan/haoqi/bottle/ui/user/RegistActivity.java
+++ app/src/main/java/com/zhan/haoqi/bottle/ui/user/RegistActivity.java
@@ -46,23 +46,7 @@ import butterknife.OnClick;
  */
 
 public class RegistActivity extends Activity {
-
-
     private static final int REQUEST_CROP_IMAGE = 3;
-    @BindView(R.id.action_bar_middle_text)
-    TextView actionBarMiddleText;
-    @BindView(R.id.user_avatar)
-    ImageView userAvatar;
-    @BindView(R.id.action_bar_right_text)
-    TextView actionBarRightText;
-    @BindView(R.id.birthday)
-    TextView birthday;
-    @BindView(R.id.nick_name)
-    EditText nickName;
-    @BindView(R.id.pwd)
-    EditText pwd;
-    @BindView(R.id.repwd)
-    EditText repwd;
 
     private String uploadimgPath;
 
@@ -72,134 +56,130 @@ public class RegistActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
-        user = new User();
-        user.birthday = "1990-06-01";
-        user.gender = -1;
         ButterKnife.bind(this);
-        actionBarMiddleText.setText("注册");
-        birthday.setText("生日(" + user.birthday + ")");
+        ((TextView) findViewById(R.id.action_bar_middle_text)).setText("注册");
     }
 
-    @OnClick({R.id.back, R.id.submit, R.id.user_avatar, R.id.birthday, R.id.female, R.id.male})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.back:
-                finish();
-                break;
-            case R.id.submit:
-                submitRegist();
-                break;
-            case R.id.user_avatar:
-                showSelectImgDialog();
-                break;
-            case R.id.birthday:
-                String[] date = (TextUtils.isEmpty(user.birthday) ? "1990-6-1" : user.birthday).split("-");
-                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        String yy_mm_dd = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
-                        birthday.setText(yy_mm_dd);
-                        user.birthday = yy_mm_dd;
-                    }
-                }, Integer.parseInt(date[0]), Integer.parseInt(date[1]) - 1, Integer.parseInt(date[2])).show();
-
-                break;
-            case R.id.male:
-                findViewById(R.id.male).setBackgroundColor(getResources().getColor(R.color.app_color));
-                findViewById(R.id.female).setBackgroundColor(getResources().getColor(R.color.white));
-                user.gender = 1;
-                MaterialDialogUtil.showDialogTip(this, "注意，注册成功后性别无法修改");
-                break;
-            case R.id.female:
-                findViewById(R.id.female).setBackgroundColor(getResources().getColor(R.color.app_color));
-                findViewById(R.id.male).setBackgroundColor(getResources().getColor(R.color.white));
-                user.gender = 0;
-                MaterialDialogUtil.showDialogTip(this, "注意，注册成功后性别无法修改");
-                break;
-        }
-    }
+//    @OnClick({R.id.back, R.id.submit, R.id.user_avatar, R.id.birthday, R.id.female, R.id.male})
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.back:
+//                finish();
+//                break;
+//            case R.id.submit:
+//                submitRegist();
+//                break;
+//            case R.id.user_avatar:
+//                showSelectImgDialog();
+//                break;
+//            case R.id.birthday:
+////                String[] date = (TextUtils.isEmpty(user.birthday) ? "1990-6-1" : user.birthday).split("-");
+////                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+////                    @Override
+////                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+////                        String yy_mm_dd = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+////                        birthday.setText(yy_mm_dd);
+////                        user.birthday = yy_mm_dd;
+////                    }
+////                }, Integer.parseInt(date[0]), Integer.parseInt(date[1]) - 1, Integer.parseInt(date[2])).show();
+//
+//                break;
+//            case R.id.male:
+//                findViewById(R.id.male).setBackgroundColor(getResources().getColor(R.color.app_color));
+//                findViewById(R.id.female).setBackgroundColor(getResources().getColor(R.color.white));
+//                user.gender = 1;
+//                MaterialDialogUtil.showDialogTip(this, "注意，注册成功后性别无法修改");
+//                break;
+//            case R.id.female:
+//                findViewById(R.id.female).setBackgroundColor(getResources().getColor(R.color.app_color));
+//                findViewById(R.id.male).setBackgroundColor(getResources().getColor(R.color.white));
+//                user.gender = 0;
+//                MaterialDialogUtil.showDialogTip(this, "注意，注册成功后性别无法修改");
+//                break;
+//        }
+//    }
 
     private PopupWindow submit;
 
     private void submitRegist() {
 
-        String phone = getIntent().getStringExtra("phone");
-
-        String nickNameStr = nickName.getText().toString().trim();
-        if (TextUtils.isEmpty(nickNameStr)) {
-            To.show("昵称不能为空");
-            return;
-        }
-        if (nickNameStr.length() > 16) {
-            To.show("昵称不能超过16位");
-            return;
-        }
-
-        if (user.gender < 0) {
-            To.show("请选择性别");
-            return;
-        }
-
-        String pwdString = pwd.getText().toString();
-
-        if (TextUtils.isEmpty(pwdString.trim())) {
-            To.show("密码不能为空");
-            return;
-        }
-
-
-        if (pwdString.trim().length() < 6) {
-            To.show("密码太短");
-            return;
-        }
-
-        String repwdStr = repwd.getText().toString();
-
-        if (!pwdString.equals(repwdStr)) {
-            To.show("两次输入的密码不一致");
-            return;
-        }
-
-        submit = To.showPop(this, R.id.submit, "正在提交注册...");
-
-        File avatarFile = null;
-        if (!TextUtils.isEmpty(uploadimgPath)) {
-            avatarFile = new File(uploadimgPath);
-        }
-        RequestParam param = new RequestParam();
-        param.put("mobile", phone);
-        param.put("password", pwdString);
-        param.put("nick_name", nickNameStr);
-        param.put("gender", user.gender);
-        param.put("birthday", user.birthday);
-        if (avatarFile != null && avatarFile.exists()) {
-            param.put("avatar", avatarFile);
-        }
-        UserManager.getInstance().regist(param, new BaseSubscriber<JSONObject>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        super.onError(e);
-                        HttpError he = (HttpError) e;
-                        To.dismiss(submit);
-                    }
-
-                    @Override
-                    public void onNext(JSONObject o) {
-                        To.dismiss(submit);
-                        To.show("注册成功！");
-                        setResult(RESULT_OK);
-                        finish();
-                        UserManager.getInstance().praseAndSave(o);
-                        return;
-                    }
-                }
-
-        );
+//        String phone = getIntent().getStringExtra("phone");
+//
+//        String nickNameStr = nickName.getText().toString().trim();
+//        if (TextUtils.isEmpty(nickNameStr)) {
+//            To.show("昵称不能为空");
+//            return;
+//        }
+//        if (nickNameStr.length() > 16) {
+//            To.show("昵称不能超过16位");
+//            return;
+//        }
+//
+//        if (user.gender < 0) {
+//            To.show("请选择性别");
+//            return;
+//        }
+//
+//        String pwdString = pwd.getText().toString();
+//
+//        if (TextUtils.isEmpty(pwdString.trim())) {
+//            To.show("密码不能为空");
+//            return;
+//        }
+//
+//
+//        if (pwdString.trim().length() < 6) {
+//            To.show("密码太短");
+//            return;
+//        }
+//
+//        String repwdStr = repwd.getText().toString();
+//
+//        if (!pwdString.equals(repwdStr)) {
+//            To.show("两次输入的密码不一致");
+//            return;
+//        }
+//
+//        submit = To.showPop(this, R.id.submit, "正在提交注册...");
+//
+//        File avatarFile = null;
+//        if (!TextUtils.isEmpty(uploadimgPath)) {
+//            avatarFile = new File(uploadimgPath);
+//        }
+//        RequestParam param = new RequestParam();
+//        param.put("mobile", phone);
+//        param.put("password", pwdString);
+//        param.put("nick_name", nickNameStr);
+//        param.put("gender", user.gender);
+//        param.put("birthday", user.birthday);
+//        if (avatarFile != null && avatarFile.exists()) {
+//            param.put("avatar", avatarFile);
+//        }
+//        UserManager.getInstance().regist(param, new BaseSubscriber<JSONObject>() {
+//                    @Override
+//                    public void onCompleted() {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        super.onError(e);
+//                        HttpError he = (HttpError) e;
+//                        To.dismiss(submit);
+//                    }
+//
+//                    @Override
+//                    public void onNext(JSONObject o) {
+//                        To.dismiss(submit);
+//                        To.show("注册成功！");
+//                        setResult(RESULT_OK);
+//                        finish();
+//                        UserManager.getInstance().praseAndSave(o);
+//                        return;
+//                    }
+//                }
+//
+//        );
 
     }
 
@@ -241,7 +221,7 @@ public class RegistActivity extends Activity {
                 chooseSmalPic();
             }
         } else if (requestCode == REQUEST_CROP_IMAGE && resultCode == RESULT_OK) {
-            ImageShowUtil.display(this, "file://" + uploadimgPath, userAvatar, false);
+            //ImageShowUtil.display(this, "file://" + uploadimgPath, userAvatar, false);
         }
     }
 
@@ -269,8 +249,4 @@ public class RegistActivity extends Activity {
         startActivityForResult(intent, REQUEST_CROP_IMAGE);
     }
 
-
-    @OnClick(R.id.birthday)
-    public void onClick() {
-    }
 }
